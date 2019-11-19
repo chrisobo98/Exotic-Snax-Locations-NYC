00c5ea172f76db126df8993827fdf14b3183436b
chrisobo98 <christopherbermudez98@gmail.com>
2019-11-18 22:09:05 -0500

    implemented map view with react-native-maps, have small 200x200 map in the middle of the screen

--- screens/Map.js
+++ screens/Map.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
 export default class Map extends React.Component {
   render(){
     return (
-      <View style={styles.container}>
-        <Text>Map</Text>
-      </View>
+        <View style={styles.container}>
+            <MapView style={styles.map}
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+        >
+            </MapView>      
+        </View>
     );
   }
 }
-const styles = StyleSheet.create({
+
+
+  
+  const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: 200,
+    height : 200
   },
 });
